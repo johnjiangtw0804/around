bf276c67cddce65730b6ca940232f771caafb127
JonTheJi <johnjiangtw0804@gmail.com>
2018-06-23 21:20:59 -0700

    ES

--- service/main.go
+++ service/main.go
@@ -15,7 +15,10 @@ const (
 	DISTANCE = "200km"
 	TYPE     = "post"
 	INDEX    = "around"
-
+        // Needs to update
+        PROJECT_ID = "around-stunning-symbol-206502"
+        BT_INSTANCE = "around-post"
+        // Needs to update this URL if you deploy it to cloud.
 	ES_URL = "http://35.224.32.198:9200"
 )
 
